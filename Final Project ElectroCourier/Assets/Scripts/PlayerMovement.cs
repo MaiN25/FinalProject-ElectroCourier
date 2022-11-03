12110741f2d3b66de1179757c51ed91615cffdd5
Cherry-Soup <66222914+Cherry-Soup@users.noreply.github.com>
2022-11-03 16:21:45 -0500

    Player Prefab + Movement Script

--- Final Project ElectroCourier/Assets/Scripts/PlayerMovement.cs
+++ Final Project ElectroCourier/Assets/Scripts/PlayerMovement.cs
@@ -4,6 +4,18 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    private float movementDirection;
+
+    [Header("Character Attributes:")]
+    public float MOVEMENT_SPEED;
+    public float JUMP_SPEED;
+    [Space]
+
+    [Header("References:")]
+    public Rigidbody2D rb;
+    
+    bool canJump;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +25,22 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Move();
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump();
+        }
+    }
+
+    void Move()
+    {
+        movementDirection = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(movementDirection * MOVEMENT_SPEED, rb.velocity.y); 
+    }
+
+    void Jump()
+    {
+        rb.AddForce(new Vector2(0, JUMP_SPEED), ForceMode2D.Impulse);
     }
 }
